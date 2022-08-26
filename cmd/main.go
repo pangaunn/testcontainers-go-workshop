065ca66e83a5bf213fe1e69838c719bb893db541
infernalslam <infernal-slam@hotmail.com>
2022-08-26 15:24:24 +0700

    feat: add gin golang

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pangaunn/testcontainers-go-workshop/cmd/handler"
+)
 
 func main() {
-	fmt.Println("Hello....")
+	port := ":3000"
+
+	r := gin.Default()
+	r.GET("/healthcheck", handler.Test)
+
+	r.Run(port)
 }
