23207f8f9639d8ddc75e8dae3637e99901f772c2
infernalslam <infernal-slam@hotmail.com>
2022-08-28 17:04:57 +0700

    feat: update data for es

--- seed/es/es_init.sh
+++ seed/es/es_init.sh
@@ -5,7 +5,7 @@ set -e
 ES=http://elasticsearch:9200
 until $(curl --output /dev/null --silent --head --fail $ES); do
     echo 'waiting for elasticsearch...'
-    sleep 3
+    sleep 5
 done
 
 
@@ -13,3 +13,5 @@ curl -X POST -u elastic:changeme -H 'Content-Type: application/json' $ES/_templa
 curl -X DELETE -u elastic:changeme -H 'Content-Type: application/json' $ES/books_temp
 curl -X PUT -u elastic:changeme -H 'Content-Type: application/json' $ES/books_temp
 curl -X POST -u elastic:changeme -H 'Content-Type: application/json' $ES/books_temp/_alias/books
+
+curl -XPOST http://elasticsearch:9200/books/_bulk?pretty -H "Content-Type: application/json" --data-binary @/usr/local/bin/data.json
