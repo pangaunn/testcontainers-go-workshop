7518c03b7aaa177dbc6fdcdc52d0f9ce9d408032
infernalslam <infernal-slam@hotmail.com>
2022-08-28 17:10:38 +0700

    feat: set temp

--- seed/es/es_init.sh
+++ seed/es/es_init.sh
@@ -9,7 +9,7 @@ until $(curl --output /dev/null --silent --head --fail $ES); do
 done
 
 
-curl -X POST -u elastic:changeme -H 'Content-Type: application/json' $ES/_template/books_temp?include_type_name=true -d "{\"index_patterns\":[\"books_*\"],\"mappings\":{\"_doc\":{\"_source\":{\"enabled\":true},\"dynamic\":\"false\",\"properties\":{\"id\":{\"type\":\"integer\"},\"name\":{\"type\":\"text\",\"analyzer\":\"rebuilt_thai\"},\"price\":{\"type\":\"integer\"},\"author\":{\"type\":\"text\",\"analyzer\":\"rebuilt_thai\"},\"description\":{\"type\":\"text\",\"analyzer\":\"rebuilt_thai\"},\"imageURL\":{\"type\":\"text\"}}}},\"settings\":{\"number_of_shards\":\"1\",\"auto_expand_replicas\":\"0-all\",\"max_result_window\":\"5000\",\"analysis\":{\"filter\":{\"thai_stop\":{\"type\":\"stop\",\"stopwords\":\"_thai_\"}},\"analyzer\":{\"rebuilt_thai\":{\"tokenizer\":\"thai\",\"filter\":[\"lowercase\",\"asciifolding\",\"decimal_digit\",\"thai_stop\"]}}}}}"
+curl -X POST -u elastic:changeme -H 'Content-Type: application/json' $ES/_template/books_temp?include_type_name=true --data-binary @/usr/local/bin/books_temp.json
 curl -X DELETE -u elastic:changeme -H 'Content-Type: application/json' $ES/books_temp
 curl -X PUT -u elastic:changeme -H 'Content-Type: application/json' $ES/books_temp
 curl -X POST -u elastic:changeme -H 'Content-Type: application/json' $ES/books_temp/_alias/books
