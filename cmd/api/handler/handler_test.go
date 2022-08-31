7807237b65128e8a8a86fef8f72fa5d0d579c76a
infernalslam <infernal-slam@hotmail.com>
2022-08-31 10:26:55 +0700

    test: create book

--- cmd/api/handler/handler_test.go
+++ cmd/api/handler/handler_test.go
@@ -4,6 +4,7 @@
 package handler_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -45,6 +46,39 @@ var _ = Describe("Handler", func() {
 			Description: "Harry Potter and the Sorcerer's Stone",
 			ImageURL:    "http://www.adviceforyou.co.th/blog/wp-content/uploads/2011/12/harry-potter.jpeg",
 		}
+		Expect(b).To(Equal(expected))
+	})
+
+	It("Create Book Should return OK 200", func() {
+
+		payload := book.NewBookRequest{
+			Name:        "the snowman (harry hole)",
+			Price:       320,
+			Author:      "Jo Nesbø",
+			Description: "the snowman",
+			ImageURL:    "https://images-na.ssl-images-amazon.com/images/I/51kgjrXdYKL._SX325_BO1,204,203,200_.jpg",
+		}
+
+		data, _ := json.Marshal(payload)
+		reader := bytes.NewReader(data)
+
+		req, _ := http.NewRequest(http.MethodPost, API_URL+"/api/v1/book", reader)
+		w := httptest.NewRecorder()
+		Engine.ServeHTTP(w, req)
+
+		expected := book.BookResponse{
+			ID:          3,
+			Name:        "the snowman (harry hole)",
+			Price:       320,
+			Author:      "Jo Nesbø",
+			Description: "the snowman",
+			ImageURL:    "https://images-na.ssl-images-amazon.com/images/I/51kgjrXdYKL._SX325_BO1,204,203,200_.jpg",
+		}
+
+		res, _ := ioutil.ReadAll(w.Body)
+		var b book.BookResponse
+		json.Unmarshal(res, &b)
+
 		Expect(b).To(Equal(expected))
 	})
 
