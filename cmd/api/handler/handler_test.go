0f8c6b6e39567cf63991fef9e7695a732d3564dd
infernalslam <infernal-slam@hotmail.com>
2022-08-31 10:10:15 +0700

    test: add test get by id

--- cmd/api/handler/handler_test.go
+++ cmd/api/handler/handler_test.go
@@ -4,12 +4,14 @@
 package handler_test
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/pangaunn/testcontainers-go-workshop/pkg/book"
 )
 
 var API_URL = "http://localhost:3000"
@@ -23,6 +25,27 @@ var _ = Describe("Handler", func() {
 		res, _ := ioutil.ReadAll(w.Body)
 		Expect(w.Code).To(Equal(http.StatusOK))
 		Expect(string(res)).To(Equal("\"OK\""))
+	})
+
+	It("Get Book By ID Should return OK 200", func() {
+		req, _ := http.NewRequest(http.MethodGet, API_URL+"/api/v1/book/1", nil)
+		w := httptest.NewRecorder()
+		Engine.ServeHTTP(w, req)
+
+		res, _ := ioutil.ReadAll(w.Body)
+		var b book.BookResponse
+		json.Unmarshal(res, &b)
+
+		Expect(w.Code).To(Equal(http.StatusOK))
+		expected := book.BookResponse{
+			ID:          1,
+			Name:        "Book 1: Harry Potter and the Sorcerer's Stone",
+			Price:       530,
+			Author:      "JK rowling",
+			Description: "Harry Potter and the Sorcerer's Stone",
+			ImageURL:    "http://www.adviceforyou.co.th/blog/wp-content/uploads/2011/12/harry-potter.jpeg",
+		}
+		Expect(b).To(Equal(expected))
 	})
 
 })
